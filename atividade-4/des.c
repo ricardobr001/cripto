916ee72fb6ab28c58f8753aa9f94bdfc93d66430
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-10-06 13:16:42 -0300

    des.c

---
+++ atividade-4/des.c
@@ -0,0 +1,18 @@
+/* ==========================================================================
+ * Universidade Federal de São Carlos - Campus Sorocaba
+ * Disciplina: Introdução à Criptografia
+ * Profa. Yeda Venturini
+ *
+ * Homework 03 - Espionagem
+ *
+ * Aluno: Ricardo Mendes Leal Junior    RA: 562262
+ * Aluno: Lucas Alexandre Occaso        RA: 
+ * ========================================================================== */
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
