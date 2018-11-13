38412b07524c05bbb523871f83bf141b2d7694f9
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-11-13 20:12:08 -0200

    funcionando

--- atividade-5/quebraRSA.c
+++ atividade-5/quebraRSA.c
@@ -14,8 +14,6 @@ unsigned long long int phi(unsigned long long int p, unsigned long long int q);
 void breakRSA(unsigned long long int produtoPrimos, unsigned long long int chavePub, unsigned long long int mensagem);
 unsigned long long int inversoModular(unsigned long long int a, unsigned long long int b);
 unsigned long long int eleva(unsigned long long int a, unsigned long long int b, unsigned long long int mod);
-unsigned long long int elevaDois(int vezes);
-unsigned long long int elevaReal(unsigned long long int a, unsigned long long int n);
 
 // http://mathforum.org/library/drmath/view/65801.html <- encontrando o primo
 unsigned long long int x(unsigned long long int numero, unsigned long long int N);
@@ -33,16 +31,20 @@ int main()
     return 0;
 }
 
+/* Função que calcula o valor de phi para dois primos p e q */
 unsigned long long int phi(unsigned long long int p, unsigned long long int q)
 {
     return (p - 1) * (q - 1);
 }
 
+/* Função que quebra o RSA, encontrando os dois primos que formam N
+ * Depois encontra a chave privada e descriptografa o mensagem
+ */
 void breakRSA(unsigned long long int produtoPrimos, unsigned long long int chavePub, unsigned long long int mensagem)
 {
     unsigned long long int i = 1, primeiroPrimo, segundoPrimo, resPhi, chavePriv;
 
-    primeiroPrimo = d(i, produtoPrimos);
+    primeiroPrimo = d(i, produtoPrimos); // Chamando a função d para i = 1
 
     while(1)
     {
@@ -55,28 +57,26 @@ void breakRSA(unsigned long long int produtoPrimos, unsigned long long int chave
         // Se nao, não encontrou o primo e procura de novo incrementando o i
         else
         {
-            primeiroPrimo = d(i, produtoPrimos);
+            primeiroPrimo = d(i, produtoPrimos); // Para i > 1
         }
     }
 
     // Para encontrar o segundo primo, basta dividir o produto dos primos pelo primeiro primo, assim teremos o segundo
     segundoPrimo = produtoPrimos / primeiroPrimo;
-    resPhi = phi(primeiroPrimo, segundoPrimo);
-    chavePriv = inversoModular(chavePub, resPhi);
+    resPhi = phi(primeiroPrimo, segundoPrimo); // Calculando o phi do primeiro e segundo primo
+    chavePriv = inversoModular(chavePub, resPhi); // encontrando a chave privada (inverso modular do resultado da função phi)
 
-    printf("primo 1 = %lld\tprimo 2 = %lld\n", primeiroPrimo, segundoPrimo);
-    printf("totien = %lld\n", resPhi);
+    // printf("primo 1 = %lld\tprimo 2 = %lld\n", primeiroPrimo, segundoPrimo);
+    // printf("totien = %lld\n", resPhi);
 
+    // Descriptografando a mensagem
     printf("%lld\n", eleva(mensagem, chavePriv, produtoPrimos));
-    // printf("%lld\n", )
-    
-
 }
 
 unsigned long long int inversoModular(unsigned long long int a, unsigned long long int p)
 {
-    unsigned long long int novo = 1, velho = 0, q = p, r, h;
-    unsigned long long int pos = 0;
+    unsigned long long novo = 1, velho = 0, q = p, r, h;
+    int pos = 0;
 
     while (a > 0)
     {
@@ -89,66 +89,22 @@ unsigned long long int inversoModular(unsigned long long int a, unsigned long lo
         a = r;
         pos = !pos;
     }
-    // if (pos)
-    // {
-    //     printf("%d N\n", q);
-    // }
-    // else
-    // {
-    //     printf("%d %d\n", q, (p - velho));
-    // }
 
-    return (p - velho);
+    if (pos)
+    {
+        return velho;
+    }
+    else
+    {
+        return p - velho;
+    }
 }
 
-unsigned long long int eleva(unsigned long long int a, unsigned long long int b, unsigned long long int mod)
+unsigned long long int eleva(unsigned long long int num, unsigned long long int vezes, unsigned long long int mod)
 {
     unsigned long long int i;
-    unsigned long long int res = a, aux;
-    // int i, mask = 1;
-
-    printf("m = %lld, priv = %lld, mod = %lld\n", a, b, mod);
-
-
-    if (b == 0)
-    {
-        return 1;
-    }
-
-    for (i = 1 ; i < b ; i++)
-    {
-        // printf("res = %llu\n", res);
-        res = res * a;
-        res = res % mod;
-    }
-
-    // printf("i = %lld\n", i);
-
-    // for (i = 0 ; i < 64 ; i++)
-    // {
-    //     aux = (b >> i) & 1;
-
-    //     if (aux)
-    //     {
-    //         // printf("pow(2,%d) = %lld\n", i, elevaDois(i));
-    //         printf("pow(%lld,%lld)=%lld, mod %lld = %lld\n", a, elevaDois(i), elevaReal(a, elevaDois(i)), mod, elevaReal(a, elevaDois(i)) %mod);
-    //         // 436 ^ 8 > 2^64 -- overflow!
-    //         res = res * elevaReal(a, elevaDois(i));
-    //         res = res % mod;
-    //     }
-    // }
-
-    // for (i = 1 ; i < b ; i++)
-    // {
-    //     res = res * a;
-    // }
-    return res;
-}
-
-unsigned long long int elevaDois(int vezes)
-{
-    int i;
-    unsigned long long int res = 2;
+    unsigned long long int res = num;
+    // printf("m = %lld, priv = %lld, mod = %lld\n", num, vezes, mod);
 
     if (vezes == 0)
     {
@@ -157,24 +113,8 @@ unsigned long long int elevaDois(int vezes)
 
     for (i = 1 ; i < vezes ; i++)
     {
-        res = res * 2;
-    }
-
-    return res;
-}
-
-unsigned long long int elevaReal(unsigned long long int a, unsigned long long int n)
-{
-    unsigned long long int i, res = a;
-
-    if (n == 0)
-    {
-        return 1;
-    }
-
-    for (i = 1 ; i < n ; i++)
-    {
-        res = res * a;
+        res = res * num;
+        res = res % mod;
     }
 
     return res;
