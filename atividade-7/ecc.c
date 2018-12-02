af9593a463dac0f5e78bcea96de8280957564d91
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-12-02 11:26:41 -0200

    ecc chamando soma

--- atividade-7/ecc.c
+++ atividade-7/ecc.c
@@ -11,8 +11,55 @@
 
 #include <stdio.h>
 
+typedef struct ponto
+{
+    long long int x, y;
+} Ponto;
+
+Ponto ecc(int n, long long int a, long long int p, Ponto g);
+Ponto somaPontos(Ponto g);
+
 int main()
 {
+    Ponto g, res;
+    int n;
+    long long int a, p;
+
+    scanf("%d", &n);
+
+    // Enquanto não ler 0, continua...
+    while (n) 
+    {
+        scanf("%lld %lld %lld %lld", &a, &p, &g.x, &g.y);
+        res = ecc(n, a, p, g);
+        printf("%lld %lld\n", res.x, res.y);
+        scanf("%d", &n);
+    }
     
     return 0;
 }
+
+Ponto ecc(int n, long long int a, long long int p, Ponto g)
+{
+    int i;
+    Ponto res = g;
+
+    // Efetua a soma n vezes
+    for (i = 0 ; i < n ; i++)
+    {
+        // res se torna o ponto g após a soma
+        res = somaPontos(res);
+    }
+
+    return res;
+}
+
+Ponto somaPontos(Ponto g)
+{
+    long long int lambda;
+    Ponto inf, q, r;
+    inf.x = 0; inf.y = 0;
+
+    // Primeiro é necessário calcular o lambda para encontrar o ponto q
+}
+
